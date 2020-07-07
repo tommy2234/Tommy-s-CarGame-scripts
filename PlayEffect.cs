8cc5f3b9deffc84a22069c9fe04f8e6b9e37c363
tommy2234 <67837599+tommy2234@users.noreply.github.com>
2020-07-07 19:16:17 +0800

    控制音效的撥放(開始、結束撥放)

---
+++ PlayEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayEffect : MonoBehaviour
+{
+    public int MyID;
+    private void Awake()
+    {
+        this.enabled = false;    //暫時關閉腳本
+    }
+    private void FixedUpdate()
+    {
+        //如果音樂片段撥放完畢且沒暫停
+        if(!GetComponent<AudioSource>().isPlaying && !PauseCtl.self.PauseBool)
+        {
+            EffectPlayer.self.audios.RemoveAt(MyID);
+            EffectPlayer.self.ResetID();
+            Destroy(gameObject);     //就刪除物件
+        }
+    }
+}
