260a55d1cfae0de1bec5bf7a0be81514ed95be70
tommy2234 <67837599+tommy2234@users.noreply.github.com>
2020-07-07 19:28:21 +0800

    場景切換的過場動畫

---
+++ LoadingSceneCtl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneCtl : MonoBehaviour
+{
+    public static LoadingSceneCtl self;
+    private void Awake()
+    {
+        self = this;
+    }
+    public void Loading()
+    {
+        GetComponent<Animation>().Play("FadeOut");
+        Invoke("ChangeScene",0.5f);
+    }
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(0);      //更換到編號0的場景
+        //SceneManager.LoadScene("Main);
+    }
+}
