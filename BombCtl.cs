757395c89f2a4ea854f5c0ae2ca3c21e4c55ba1b
tommy2234 <67837599+tommy2234@users.noreply.github.com>
2020-07-07 19:41:25 +0800

    控制生產出的地雷

---
+++ BombCtl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+//控制地雷
+public class BombCtl : MonoBehaviour
+{
+    private float MaxDistance = 13f;  
+    void FixedUpdate()
+    {
+        float TrueSpeed = -carctl.self.bgspeed/2.35f;       //算出地雷真正該移動的速度
+        transform.position += new Vector3(0, TrueSpeed, 0);
+        //若這個物件位置 和 地雷產生器位置 距離超過 特定距離
+        if (Vector3.Distance(transform.position, SpawnEnemyCtl.self.transform.position) > MaxDistance)
+            Destroy(gameObject);   
+    }   
+
+}
