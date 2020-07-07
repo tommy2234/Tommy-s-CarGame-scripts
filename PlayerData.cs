a82eec94676e107b5e62b6d43e750cea6911ba20
tommy2234 <67837599+tommy2234@users.noreply.github.com>
2020-07-07 19:20:20 +0800

    玩家資料
    關卡數、過關或輸、遊戲是否已停止

---
+++ PlayerData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//玩家資料
+[Serializable]     //將資料序列化
+public class PlayerData
+{ 
+    public bool Stop;   //遊戲是否已停止
+    public bool Lose;   //是否輸了
+    public int Level;   //關卡數
+}
