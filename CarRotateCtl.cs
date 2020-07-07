1ff3a9f2724872f1c4d58ada0bfd07b5db4697e7
tommy2234 <67837599+tommy2234@users.noreply.github.com>
2020-07-07 19:05:01 +0800

    控制玩家汽車旋轉(打滑)

---
+++ CarRotateCtl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarRotateCtl : MonoBehaviour
+{
+    private float CutSpeed = 0.015f;
+    private float RotateSpeed = 20f;
+    private float xmovespeed = 0.05f;  //打滑時車左右移動速度
+    private int Direction;
+    private void Awake()
+    {
+        this.enabled = false;
+    }
+    private void FixedUpdate()
+    {
+        carctl.self.bgspeed=Mathf.Clamp( carctl.self.bgspeed -= CutSpeed,0,1);
+        transform.Rotate(0, 0, RotateSpeed*Direction);
+        carctl.self.carposx -= xmovespeed*Direction;
+
+    }
+    public void Hit(int dir)
+    {
+        this.enabled = true;
+        Direction = dir;
+    }
+}
