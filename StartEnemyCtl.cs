dfef88ab04e40973ad52ca11e824b383d147eb48
tommy2234 <67837599+tommy2234@users.noreply.github.com>
2020-07-07 19:08:38 +0800

    控制開場時的敵人車輛

---
+++ StartEnemyCtl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//控制初始敵人移動
+public class StartEnemyCtl : MonoBehaviour       
+{
+    private float speed = 0.8f;         //敵人最大車速
+    void FixedUpdate()
+    {    
+        transform.position += new Vector3(0, speed, 0);
+    }
+}
