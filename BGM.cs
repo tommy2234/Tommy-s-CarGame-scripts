d88c559afde6a3093b1edff5707788da482e4d1d
tommy2234 <67837599+tommy2234@users.noreply.github.com>
2020-07-07 19:14:15 +0800

    播放背景音樂

---
+++ BGM.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//BGM控制器
+public class BGM : MonoBehaviour
+{
+    public static BGM self;
+    private void Awake()
+    {
+        self = this;
+    }
+
+    AudioSource aud;
+    private const string BGMPath = "Audios/";
+
+    private void Start()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+    private void PlayBGM(string name)
+    {
+        aud.clip = Resources.Load<AudioClip>(BGMPath + name);
+        aud.Play();         //真正播放指令
+    }
+}
