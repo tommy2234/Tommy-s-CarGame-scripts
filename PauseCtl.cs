bdd9b72a11fd860da2d85ac8aa87184a852ebe9e
tommy2234 <67837599+tommy2234@users.noreply.github.com>
2020-07-07 19:39:55 +0800

    設計暫停鍵、跳出遊戲的按鍵

---
+++ PauseCtl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//控制暫停
+public class PauseCtl : MonoBehaviour
+{
+    public bool PauseBool = false;  //暫停/開始
+    public static PauseCtl self;
+    private void Awake()
+    {
+        self = this;
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))  //若按下大Enter鍵
+        {
+            PauseBool = !PauseBool;
+            Pause();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+    void Pause()
+    {
+        Time.timeScale = PauseBool ? 0 : 1;
+        EffectPlayer.self.Pause(PauseBool);
+    }
+    
+}
