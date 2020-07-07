e547473f79bcc619800523c7f263cf9841f8e341
tommy2234 <67837599+tommy2234@users.noreply.github.com>
2020-07-07 19:24:31 +0800

    顯示玩家的車速(公里)

---
+++ KMctl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KMctl : MonoBehaviour
+{
+    private int maxspeed = 200;   //車最高速(KM)
+    void FixedUpdate()
+    {
+        int km = (int)(carctl.self.bgspeed * maxspeed);   //計算最大公里數的比例(當下時速)
+        //將公里數的文字設成當下時數 並加上KM/H
+        GetComponent<Text>().text = km.ToString() + " KM/H";    
+        
+    }
+}
